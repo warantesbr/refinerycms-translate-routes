312d1f9f7aa6b030bf5580083a1c55ed4f36a85a
Luis Eduardo Rodríguez Mendo <luis.mendo@the-cocktail.com>
2012-11-02 14:42:40 +0100

    Adding tests for routing and helpers.

---
+++ spec/helpers/refinery/translate_routes/helpers_spec.rb
@@ -0,0 +1,42 @@
+require "spec_helper"
+
+module Refinery
+  describe "TranslateRoutes" do
+
+    let(:host){ "dummy.net" }
+    let(:page_title){ "dummy page" }
+    let(:page){ Factory.create :page, title: page_title }
+
+    context "helpers" do
+
+      context "when locale is set to en" do
+
+        before{ ::I18n.locale = :en }
+
+        context "page path helper output" do
+          it { refinery.page_path(page).should eq("/pages/#{page.slug}") }
+        end
+
+        context "page url helper output" do
+          it { refinery.page_url(page, host: host).should eq("http://#{host}/pages/#{page.slug}") }
+        end
+
+      end
+
+      context "when locale is set to es" do
+
+        before{ ::I18n.locale = :es }
+
+        context "page path helper output" do
+          it { refinery.page_path(page).should eq("/es/paginas/#{page.slug}") }
+        end
+
+        context "page url helper output" do
+          it { refinery.page_url(page, host: host).should eq("http://#{host}/es/paginas/#{page.slug}") }
+        end
+
+      end
+
+    end
+  end
+end
