650ee3d3b0748a034e6fbbb73267b9c25ed70099
David J. Brenes <david.brenes@simplelogica.net>
2013-01-18 15:09:32 +0100

    Now we test that url helper prints the right utl

--- spec/requests/refinery/translate_routes/routing_spec.rb
+++ spec/requests/refinery/translate_routes/routing_spec.rb
@@ -4,9 +4,13 @@ include Capybara::DSL
 describe 'TranslateRoutes' do
 
   let(:page_title){ "dummy page" }
+  let(:page_title_with_url){ "dummy page with url" }
   let(:page){ Factory.create :page, title: page_title }
+  let(:page_with_url){ Factory.create :page, title: page_title_with_url }
 
-  before { Refinery::PagesController.any_instance.stub(:refinery_user_required?).and_return(false) }
+  before {
+    Refinery::PagesController.any_instance.stub(:refinery_user_required?).and_return(false)
+   }
 
   context "routing" do
 
@@ -30,6 +34,24 @@ describe 'TranslateRoutes' do
       it { page.should have_xpath("//section[@id='page']//section//h1[@id='body_content_title']", text: page_title) }
       it { page.should have_xpath("//nav[@id='menu' and @class='menu']//ul//li//a[@href='/es/#{page.slug}']", text: page_title) }
 
+    end
+
+    context "when using url helpers" do
+      context "when visit page with default path" do
+
+        before { visit "/pages/#{page_with_url.slug}" }
+
+        it { page.should have_content("/pages/#{page_with_url.slug}") }
+
+      end
+
+      context "when visit page with translated path" do
+
+        before { visit "/es/paginas/#{page.slug}" }
+
+        it { page.should have_content("/es/paginas/#{page_with_url.slug}") }
+
+      end
     end
   end
 end
