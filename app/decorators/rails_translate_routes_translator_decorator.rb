984fb5afda3642873232836b1de999b2fc6a894d
Rodrigo García Suárez <rodrigo@simplelogica.net>
2013-09-03 13:54:40 +0200

    Resangrado.

--- app/decorators/rails_translate_routes_translator_decorator.rb
+++ app/decorators/rails_translate_routes_translator_decorator.rb
@@ -74,13 +74,13 @@ RailsTranslateRoutes::Translator.module_eval do
       containers.each do |helper_container|
         helper_container.module_eval do
 
-            self.send :define_method, new_helper_name.to_sym do |*args|
-              if suffix == "url"
-                options = args.extract_options!
-                args << url_options.merge((options || {}).symbolize_keys)
-              end
-              route_set.url_helpers.send "#{old_name}_#{locale_suffix(I18n.locale)}_#{suffix}", *args
+          self.send :define_method, new_helper_name.to_sym do |*args|
+            if suffix == "url"
+              options = args.extract_options!
+              args << url_options.merge((options || {}).symbolize_keys)
             end
+            route_set.url_helpers.send "#{old_name}_#{locale_suffix(I18n.locale)}_#{suffix}", *args
+          end
 
         end
       end
